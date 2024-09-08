0a138772dbcc6116466fe5763a1f5ad89d47355a
nomstaiters <ryan.tait.6@gmail.com>
2024-09-08 03:19:29 -0400

    added sorting dictionary of character counts

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_counts = get_char_counts(text)
-    print(f"{word_count} words in text")
-    print("the counts of each character are as follows :", char_counts)
+    list_char_counts = list_of_dicts(char_counts)
+    list_char_counts.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for char in list_char_counts:
+        if char['char'].isalpha():
+            print(f"The '{char['char']}' character was found {char['num']} times")
+    print("End of report")
 
 
 def get_book_text(path):
@@ -26,5 +33,23 @@ def get_char_counts(text):
             count_dict[char] = 1
     return count_dict        
 
+#turn char counts into list of dictionaries
+def list_of_dicts(dict):
+    char_counts = []
+    for name in dict:
+        char_dict = {}
+        char_dict["char"] = name
+        char_dict["num"] = dict[name]
+        char_counts.append(char_dict)
+    return char_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+# test = "abracadabra boo"
+# count_dict = get_char_counts(test)
+# test_list = list_of_dicts(count_dict)
+# test_list.sort(reverse=True, key=sort_on)
+# print(test_list)
 
 main()
